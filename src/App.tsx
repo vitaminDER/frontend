30a74def2496512677f0d10cdb30facdda86082e
sergeji <kirr7619@gmail.com>
2025-07-06 11:19:57 +0500

    delete logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import {useEffect, useMemo, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {Book} from "./Book.tsx";
 
