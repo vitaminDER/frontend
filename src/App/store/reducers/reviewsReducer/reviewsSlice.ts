fdfd465218a6159800055a9e3bf1fcc606e89178
sergeji <kirr7619@gmail.com>
2025-07-27 09:23:08 +0200

    Закоментил моки

--- src/App/store/reducers/reviewsReducer/reviewsSlice.ts
+++ src/App/store/reducers/reviewsReducer/reviewsSlice.ts
@@ -3,29 +3,29 @@ import {createSlice} from "@reduxjs/toolkit";
 import type {ReviewsScheme} from "./reviewsScheme.ts";
 import {fetchReviews} from "./services/fetchReviews.ts";
 
-const reviewMock = [
-    {
-        personId: '1',
-        username: 'Иванов Иван',
-        comment: 'В extraReducers можно обрабатывать действия из нескольких builder-ов, но сам extraReducers принимает только один колбэк с builder. Однако внутри этого колбэка вы можете обрабатывать действия из разных слайсов или редьюсеров.',
-        createdDate: '21.11.2025',
-        evaluation: 4,
-    },
-    {
-        personId: '2',
-        username: 'Петров Петр',
-        comment: 'В extraReducers можно обрабатывать действия из нескольких builder-ов, но сам extraReducers принимает только один колбэк с builder. Однако внутри этого колбэка вы можете обрабатывать действия из разных слайсов или редьюсеров.',
-        createdDate: '02.11.2025',
-        evaluation: 3,
-    },
-    {
-        personId: '3',
-        username: 'Петров Петр',
-        comment: 'В extraReducers можно обрабатывать действия из нескольких builder-ов, но сам extraReducers принимает только один колбэк с builder. Однако внутри этого колбэка вы можете обрабатывать действия из разных слайсов или редьюсеров.',
-        createdDate: '02.11.2025',
-        evaluation: 3,
-    },
-]
+// const reviewMock = [
+//     {
+//         personId: '1',
+//         username: 'Иванов Иван',
+//         comment: 'В extraReducers можно обрабатывать действия из нескольких builder-ов, но сам extraReducers принимает только один колбэк с builder. Однако внутри этого колбэка вы можете обрабатывать действия из разных слайсов или редьюсеров.',
+//         createdDate: '21.11.2025',
+//         evaluation: 4,
+//     },
+//     {
+//         personId: '2',
+//         username: 'Петров Петр',
+//         comment: 'В extraReducers можно обрабатывать действия из нескольких builder-ов, но сам extraReducers принимает только один колбэк с builder. Однако внутри этого колбэка вы можете обрабатывать действия из разных слайсов или редьюсеров.',
+//         createdDate: '02.11.2025',
+//         evaluation: 3,
+//     },
+//     {
+//         personId: '3',
+//         username: 'Петров Петр',
+//         comment: 'В extraReducers можно обрабатывать действия из нескольких builder-ов, но сам extraReducers принимает только один колбэк с builder. Однако внутри этого колбэка вы можете обрабатывать действия из разных слайсов или редьюсеров.',
+//         createdDate: '02.11.2025',
+//         evaluation: 3,
+//     },
+// ]
 
 const initialState: ReviewsScheme = {
     reviews: [],
@@ -43,17 +43,17 @@ const reviewsSlice = createSlice<ReviewsScheme>({
                 state.loadingReviews = FetchStatus.PENDING;
             })
             .addCase(fetchReviews.fulfilled, (state, action) => {
-                // state.reviews = action.payload;
-                console.log(action.payload);
-                state.reviews = reviewMock;
+                state.reviews = action.payload;
+                // console.log(action.payload);
+                // state.reviews = reviewMock;
                 state.loadingReviews = FetchStatus.SUCCESS;
             })
             .addCase(fetchReviews.rejected, (state, action) => {
-                // state.errorReviews = action.payload?.message?.toUpperCase()
-                // state.loadingReviews = FetchStatus.REJECTED;
-                console.log(action.payload);
-                state.reviews = reviewMock;
-                state.loadingReviews = FetchStatus.SUCCESS;
+                state.errorReviews = action.payload?.message?.toUpperCase()
+                state.loadingReviews = FetchStatus.REJECTED;
+                // console.log(action.payload);
+                // state.reviews = reviewMock;
+                // state.loadingReviews = FetchStatus.SUCCESS;
             });
 
     },
